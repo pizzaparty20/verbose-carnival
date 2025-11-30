c31b29150c2b1d8429a08c9edd63cee002a70888
pizzaparty20 <jonathanseepaul2@gmail.com>
2025-11-30 17:14:26 -0500

    Add files via upload

---
+++ tree ee e.py
@@ -0,0 +1,41 @@
+import random
+def tree_display(k):
+    N = 2*k + 1
+
+    stem = '|'
+    stem_width = k // 4
+    stem_height = k // 6
+    for n in range(1, N, 2):
+        l = (N - n) // 2
+        r = l
+        c = '^'
+        ornament = 'Q'
+
+        if n==1:
+            c = '*'
+        leafs = c*n
+
+        if n>3:
+            max_ornaments = max(1, n // 12)
+
+            positions = random.sample(range(1, n - 1), max_ornaments)
+
+            for i in positions:
+
+                z = random.randint(-2,2)
+                i2 = n - i + z
+                if (leafs[i-1]==c  and  leafs[i+1] ==c):
+                    leafs = leafs[:i] + ornament + leafs[i+1:]
+                if i2>=1 and i2<=n-2:
+                    if (leafs[i2-1]==c  and  leafs[i2+1] ==c):
+                        leafs = leafs[:i2] + ornament + leafs[i2+1:]
+        print(' ' * l + leafs + ' ' * r)
+    if k >5:
+        y = (N-stem_width)//2
+        z= " "*y + stem*stem_width + " "*y
+        for i in range(stem_height):
+            print(z)
+
+while True:
+    n = int(input("Tree Height: "))
+    tree_display(n)
